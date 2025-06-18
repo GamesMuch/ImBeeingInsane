090b7685847915f467929a99f87970d8041844bc
GamesMuch <everydaycrevan@gmail.com>
2025-06-18 17:53:52 +0200

    Entire Dialogue System
    They said i should yarn, they called me crazy...

--- BeeHiveGroupProject/Assets/Engineer-Main/Scripts/OpenUI.cs
+++ BeeHiveGroupProject/Assets/Engineer-Main/Scripts/OpenUI.cs
@@ -6,4 +6,25 @@ using UnityEngine.EventSystems;
 
 public class OpenUI : MonoBehaviour
 {
+
+
+    public List<GameObject> UiObjects = new List<GameObject>();
+    List<string> UiNames = new List<string>();
+    
+    void Start()
+    {
+        //Converts the gameobjects to a list with strings
+        foreach (GameObject obj in UiObjects)
+        {
+            UiNames.Add(obj.name);
+        }
+    }
+    public void OpenUi(string name)
+    {
+        //Opens the ui with the same name
+        if (UiNames.Contains(name)){
+            int activateNumber = UiNames.IndexOf(name);
+            UiObjects[activateNumber].SetActive(true);
+        }
+    }
 }
