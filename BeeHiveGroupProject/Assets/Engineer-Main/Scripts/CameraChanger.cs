cb0ceabd1c34888719fc3789f354865a69107cbe
GamesMuch <everydaycrevan@gmail.com>
2025-07-04 11:32:23 +0200

    Camera now works, takes trigger instead of collider

--- BeeHiveGroupProject/Assets/Engineer-Main/Scripts/CameraChanger.cs
+++ BeeHiveGroupProject/Assets/Engineer-Main/Scripts/CameraChanger.cs
@@ -5,30 +5,36 @@ public class CameraChanger : MonoBehaviour
     public Vector3 offset;
     bool hasAdjustedCamera = false;
 
-    void OnCollisionEnter(Collision collision)
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (!hasAdjustedCamera && collision.gameObject.CompareTag("Floor"))
+    //    {
+    //        hasAdjustedCamera = true;
+
+    //        Vector3 pos = collision.gameObject.transform.position;
+    //        pos.y = InfoStorage.Instance.cam.transform.position.y;
+
+    //        InfoStorage.Instance.cam.transform.position = pos + offset;
+    //    }
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    hasAdjustedCamera = false;
+    //}
+    private void OnTriggerEnter(Collider other)
     {
-        if (!hasAdjustedCamera && collision.gameObject.CompareTag("Floor"))
+        if (!hasAdjustedCamera && other.gameObject.CompareTag("Floor"))
         {
             hasAdjustedCamera = true;
 
-            Vector3 pos = collision.gameObject.transform.position;
+            Vector3 pos = other.gameObject.transform.position;
             pos.y = InfoStorage.Instance.cam.transform.position.y;
 
             InfoStorage.Instance.cam.transform.position = pos + offset;
         }
     }
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
         hasAdjustedCamera = false;
     }
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("Floor"))
-    //    {
-
-    //        Vector3 pos = other.gameObject.transform.position;
-    //        pos.y = InfoStorage.Instance.cam.transform.position.y;
-    //        InfoStorage.Instance.cam.transform.position = pos + offset;
-    //    }
-    //}
 }
